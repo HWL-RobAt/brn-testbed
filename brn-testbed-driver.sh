2949357871e39476b90c808734941720149afb6b
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2012-10-18 23:13:04 +0200

    update

--- brn-testbed-driver.sh
+++ brn-testbed-driver.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 PATH=$PATH:/testbedhome/testbed/software/openwrt/backfire-x86/staging_dir/toolchain-i386_gcc-4.1.2_uClibc-0.9.30.1/usr/bin
 PATH=$PATH:/testbedhome/testbed/software/openwrt/backfire-mips/staging_dir/toolchain-mipsel_gcc-4.1.2_uClibc-0.9.30.1/usr/bin
 PATH=$PATH:/testbedhome/testbed/software/openwrt/backfire-wndr3700/staging_dir/toolchain-mips_r2_gcc-4.1.2_uClibc-0.9.30.1/usr/bin/
@@ -30,10 +29,7 @@ case "$SIGN" in
    ;;
 esac
 
-if [ "x$CLICKPATH" = "x" ]; then
-  echo "Set CLICKPATH"
-  exit 1
-fi
+KERNELBASE="/testbedhome/testbed/software/kernel-"
 
 if [ "x$BRN_TOOLS_PATH/helper" = "x" ]; then
   echo "Set BRN_TOOLS_PATH or helper is missing"
@@ -44,69 +40,55 @@ ARCHS="mips mipsel i386"
 
 for i in $ARCHS; do
   which $i-linux-uclibc-gcc > /dev/null
-  ARCHALIAS=`cat $FULLNAME | grep "#alias $i " | awk '{print $3}'`
-  BUILDALIAS=`cat $FULLNAME | grep "#build $i " | awk '{print $3}'`
 
   if [ $? -eq 0 ]; then
     echo "Found $i-linux-uclibc-gcc"
-    if [ ! -e click-brn-$ARCHALIAS ]; then
-      git clone $CLICKPATH/.git click-brn-$ARCHALIAS
-    else
-      ( cd click-brn-$ARCHALIAS; git pull )
+  fi
+  ARCHALIAS=`cat $FULLNAME | grep "#alias $i " | awk '{print $3}'`
+  BUILDALIAS=`cat $FULLNAME | grep "#build $i " | awk '{print $3}'`
+  DRIVER=`cat $FULLNAME | grep "#driver $ARCHALIAS " | awk '{print $3}'`
+
+  LINK=`cat $FULLNAME | grep "#link $ARCHALIAS " | awk '{print $3}'`
+
+  KERNELDIRS=`(cd $KERNELBASE$ARCHALIAS; ls -d linux*)`
+  for k in $KERNELDIRS; do
+    pure_k_version=`echo $k | sed "s#linux-headers-##g" | sed "s#linux-##g"`
+    if [ -f $KERNELBASE$ARCHALIAS/$k/.config ]; then
+      for d in $DRIVER; do
+        echo "Build $d for $ARCHALIAS $KERNELBASE$ARCHALIAS/$k -> $BRN_TOOLS_PATH/helper/nodes/lib/modules/$ARCHALIAS/$pure_k_version"
+        (cd $DIR/../brn-driver; KERNELPATH=$KERNELBASE$ARCHALIAS/$k/ ARCH=$BUILDALIAS COMPILER_PREFIX=$i-linux-uclibc- TARGETDIR=$BRN_TOOLS_PATH/helper/nodes/lib/modules/$ARCHALIAS/$pure_k_version sh ./brn-driver.sh build-modules $d)
+      done
     fi
+  done
 
-    if [ ! -e click-brn-$ARCHALIAS/Makefile ]; then
-      ( cd click-brn-$ARCHALIAS; TARGET=$BUILDALIAS sh ./brn-conf.sh userlevel )
-    else
-      if [ "x$RECONFIGURE" = "x1" ]; then
-        ( cd click-brn-$ARCHALIAS; touch ./configure )
-      fi
-    fi
-
-    if [ -e click-brn-$ARCHALIAS/Makefile ]; then
-      if [ "x$ELEMLISTUPDATE" = "x1" ]; then
-        ( cd click-brn-$ARCHALIAS; make elemlist )
-      else
-        if [ "x$CLEAN" = "x1" ]; then
-          ( cd click-brn-$ARCHALIAS; make clean )
-        fi
-      fi
-      ( cd click-brn-$ARCHALIAS; make )
-    fi
-
-    if [ ! "x$DISABLE_STRIP" = "x1" ]; then
-       $i-linux-uclibc-strip --strip-unneeded click-brn-$ARCHALIAS/userlevel/click
-       $i-linux-uclibc-strip --strip-unneeded click-brn-$ARCHALIAS/tools/click-align/click-align
-    fi
-
-    LINKS=`cat $FULLNAME | grep "#link $i " | awk '{print $3}'`
-
-    for l in $LINKS; do
-      if [ "x$CLEARLINKS" = "x1" ]; then
-        rm -f $BRN_TOOLS_PATH/helper/nodes/bin/click-$l $BRN_TOOLS_PATH/helper/nodes/bin/click-align-$l
-      fi
-      if [ ! -f $BRN_TOOLS_PATH/helper/nodes/bin/click-$l ]; then
-        ln -s $DIR/click-brn-$ARCHALIAS/userlevel/click $BRN_TOOLS_PATH/helper/nodes/bin/click-$l
-      fi
-      if [ ! -f $BRN_TOOLS_PATH/helper/nodes/bin/click-align-$l ]; then
-        ln -s $DIR/click-brn-$ARCHALIAS/tools/click-align/click-align $BRN_TOOLS_PATH/helper/nodes/bin/click-align-$l
+  if [ -e $BRN_TOOLS_PATH/helper/nodes/lib/modules/$ARCHALIAS/ ]; then
+    for l in $LINK; do
+      if [ ! -e $BRN_TOOLS_PATH/helper/nodes/lib/modules/$l ]; then
+        (cd $BRN_TOOLS_PATH/helper/nodes/lib/modules/; ln -s $ARCHALIAS $l)
       fi
     done
   fi
+
 done
 
+#
+#   C O N F I G   P A R T
+#
 
 #alias mips mips-wndr3700
 #alias mipsel mips
 #alias i386 x86
 
-#build mips mips2
+#build mips mips
 #build mipsel mips
 #build i386 i386
 
-#link mips mips-wndr3700
-#link mipsel mips
-#link i386 i386
-#link i386 i486
-#link i386 i586
-#link i386 i686
+#link x86 i386
+#link x86 i386
+#link x86 i486
+#link x86 i586
+#link x86 i686
+
+#driver x86 madwifi
+#driver mips madwifi
+#driver mips-wndr3700 ath
